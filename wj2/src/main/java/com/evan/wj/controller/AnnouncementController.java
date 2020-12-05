4403280b595b2129d399c408666eeefb02d2630e
Pikayue11 <73102892+Pikayue11@users.noreply.github.com>
2020-12-05 21:45:46 +0800

    announcement

---
+++ wj2/src/main/java/com/evan/wj/controller/AnnouncementController.java
@@ -0,0 +1,32 @@
+package com.evan.wj.controller;
+
+
+import com.evan.wj.bean.AllProject;
+import com.evan.wj.bean.Announcement;
+import com.evan.wj.dao.AnnouncementDAO;
+import com.evan.wj.dao.ProjectDAO;
+import com.evan.wj.result.TempleteResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AnnouncementController {
+    @Autowired
+    AnnouncementDAO announcementDAO;
+
+    @CrossOrigin
+    @GetMapping(value = "/vue-element-admin/student/dashboard/announcement_list")
+    @ResponseBody
+    public TempleteResult<Announcement> getAllGroup(@RequestParam("token") String token){
+
+        TempleteResult<Announcement> allProjectResult_t = new TempleteResult<Announcement>(20000,announcementDAO.getAllAnnouncement(token));
+
+        //AllProjectResult allProjectResult = new AllProjectResult(20000, projectDAO.getAllProject(token));
+        return allProjectResult_t;
+
+    }
+}
