d52e110818aa0f517b8ab28050786795c259213a
Pikayue11 <73102892+Pikayue11@users.noreply.github.com>
2020-12-05 18:51:32 +0800

    group6

--- wj2/src/main/java/com/evan/wj/controller/GroupController.java
+++ wj2/src/main/java/com/evan/wj/controller/GroupController.java
@@ -37,28 +37,35 @@ public class GroupController {
     public MyGroupResult getMyGroup(@RequestParam("token") String token, @RequestParam("project_id") int project_id){
         List<MyGroup> myGroups = new ArrayList<MyGroup>();
 
-        // GetGroID {id }
-        List<MyGroup_getGroId> gro_id_list = groupDAO.getAMyGroup_getGroID(token, project_id);
-        int gro_id = gro_id_list.get(0).getId();
-        // NameGender {int id, String name, String gender, List<String》 tag}
-        List<MyGroup_getNameGender> people_id_list = groupDAO.getAMyGroup_getName(gro_id);
+        MyGroup a = new MyGroup(1,"wky1","boy");
+        a.addtags("前端");
+        myGroups.add(a);
 
-        // MyGroup {int id, String name, String gender, List<String》 tag}
-        for(MyGroup_getNameGender ids:people_id_list){
-            myGroups.add(new MyGroup(ids.getId(),ids.getName(),ids.getGender()));
-        }
+//
+//        // GetGroID {id }
+//        List<MyGroup_getGroId> gro_id_list = groupDAO.getAMyGroup_getGroID(token, project_id);
+//        int gro_id = gro_id_list.get(0).getId();
+//        // NameGender {int id, String name, String gender, List<String》 tag}
+//        List<MyGroup_getNameGender> people_id_list = groupDAO.getAMyGroup_getName(gro_id);
+//
+//        // MyGroup {int id, String name, String gender, List<String》 tag}
+//        for(MyGroup_getNameGender ids:people_id_list){
+//            myGroups.add(new MyGroup(ids.getId(),ids.getName(),ids.getGender()));
+//        }
+//
+//        List<MyGroup_idTag> tags = groupDAO.gettags(gro_id);
+//        // MyGroup_idTag {int id, String tag}
+//        for (MyGroup_idTag tg: tags ){
+//            int person = tg.getId();
+//            // MyGroup {int id, String name, String gender, List<String》 tag}
+//            for (MyGroup p: myGroups){
+//                if(person == p.getId()){
+//                    p.getTags().add(tg.getTag());
+//                }
+//            }
+//        }
 
-        List<MyGroup_idTag> tags = groupDAO.gettags(gro_id);
-        // MyGroup_idTag {int id, String tag}
-        for (MyGroup_idTag tg: tags ){
-            int person = tg.getId();
-            // MyGroup {int id, String name, String gender, List<String》 tag}
-            for (MyGroup p: myGroups){
-                if(person == p.getId()){
-                    p.getTags().add(tg.getTag());
-                }
-            }
-        }
+
 
         MyGroupResult myGroupResult = new MyGroupResult(20000, myGroups);
         return myGroupResult;
