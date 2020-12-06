368f805135dfb9a9742d17fb0a216f7cfe56aa09
Pikayue11 <73102892+Pikayue11@users.noreply.github.com>
2020-12-06 11:57:53 +0800

    wanted

---
+++ wj2/src/main/java/com/evan/wj/bean/PersonalInfo_pro.java
@@ -0,0 +1,78 @@
+package com.evan.wj.bean;
+
+public class PersonalInfo_pro {
+    String name;
+    String gender;
+    String lab;
+    String skill;
+    String hope;
+    String status;
+
+    public PersonalInfo_pro(String name, String gender, String lab, String skill, String hope, String status) {
+        this.name = name;
+        this.gender = gender;
+        this.lab = lab;
+        this.skill = skill;
+        this.hope = hope;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLab() {
+        return lab;
+    }
+
+    public void setLab(String lab) {
+        this.lab = lab;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getHope() {
+        return hope;
+    }
+
+    public void setHope(String hope) {
+        this.hope = hope;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
