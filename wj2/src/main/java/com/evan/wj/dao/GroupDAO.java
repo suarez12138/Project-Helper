c0f545621016074cdab87acd9af9de5511bf863b
Pikayue11 <73102892+Pikayue11@users.noreply.github.com>
2020-12-10 22:52:44 +0800

    new2

--- wj2/src/main/java/com/evan/wj/dao/GroupDAO.java
+++ wj2/src/main/java/com/evan/wj/dao/GroupDAO.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public interface GroupDAO extends JpaRepository<User,Integer> {
 
-    @Query("select new com.evan.wj.bean.AllGroup(g.id, g.group_name, g.pre_time, g.group_status) from Gro g where g.group_status <> 'Graded' and g.project =?1")
+    @Query("select new com.evan.wj.bean.AllGroup(g.id, g.group_name, g.pre_week, g.group_status) from Gro g where g.group_status <> 'Graded' and g.project =?1")
     List<AllGroup> getAllGroup(int project_id);
 
 
