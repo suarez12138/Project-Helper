49f88a4ea8f1ffe9d1c2e0aa73aafbc8b9459b4f
Pikayue11 <73102892+Pikayue11@users.noreply.github.com>
2020-12-13 21:43:30 +0800

    new2

--- wj2/src/main/java/com/evan/wj/controller/AnnouncementController.java
+++ wj2/src/main/java/com/evan/wj/controller/AnnouncementController.java
@@ -4,6 +4,7 @@ import com.evan.wj.bean.AllProject;
 import com.evan.wj.bean.Announcement;
 import com.evan.wj.dao.AnnouncementDAO;
 import com.evan.wj.dao.ProjectDAO;
+import com.evan.wj.dao.UpPersonInfoDAO;
 import com.evan.wj.result.TempleteResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,13 +17,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AnnouncementController {
     @Autowired
     AnnouncementDAO announcementDAO;
+    @Autowired
+    UpPersonInfoDAO upPersonInfoDAO;
 
     @CrossOrigin
     @GetMapping(value = "/vue-element-admin/student/dashboard/announcement_list")
     @ResponseBody
     public TempleteResult<Announcement> getAllAnn_stu(@RequestParam("token") String token){
-
-        TempleteResult<Announcement> allProjectResult_t = new TempleteResult<Announcement>(20000,announcementDAO.getAllAnnouncement_student(token));
+        int person_id = upPersonInfoDAO.getID(token).get(0).getId();
+        TempleteResult<Announcement> allProjectResult_t = new TempleteResult<Announcement>(20000,announcementDAO.getAllAnnouncement_student(person_id));
 
         //AllProjectResult allProjectResult = new AllProjectResult(20000, projectDAO.getAllProject(token));
         return allProjectResult_t;
