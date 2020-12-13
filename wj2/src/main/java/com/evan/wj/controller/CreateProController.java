51ec9cc7d83599a078cec67b1b09f9769a432119
Pikayue11 <73102892+Pikayue11@users.noreply.github.com>
2020-12-13 17:43:02 +0800

    create_new_project

--- wj2/src/main/java/com/evan/wj/controller/CreateProController.java
+++ wj2/src/main/java/com/evan/wj/controller/CreateProController.java
@@ -2,6 +2,7 @@ package com.evan.wj.controller;
 
 import com.evan.wj.bean.AllCourse;
 import com.evan.wj.bean.All_tag_inProject;
+import com.evan.wj.bean.The_id;
 import com.evan.wj.dao.CreateProjectDAO;
 import com.evan.wj.dao.TagDAO;
 import com.evan.wj.dao.UpPersonInfoDAO;
@@ -33,41 +34,62 @@ public class CreateProController {
     }
 
     @CrossOrigin
-    @PostMapping(value = "/vue-element-admin/teacher/project/new_project")
+    @PostMapping(value = "/vue-element-admin/teacher/project/new_project_insert")
     @ResponseBody
     public Void_return create_new_project(@RequestBody NewProjectReceive newProInfo){
         int course_id = newProInfo.getCourse_id();
         int min_p = newProInfo.getProject_member_limit().get(0);
         int max_p = newProInfo.getProject_member_limit().get(1);
+        String token = newProInfo.getToken();
+        int teacher_id = upPersonInfoDAO.getID(token).get(0).getId();
+        String pro_name = newProInfo.getProject_name();
+        String bool_cross = newProInfo.getGrouping_freely();
+        String bool_force = newProInfo.getChoosable_proj();
+
+        String group_ddl = newProInfo.getPro_grouping_endDay() + " " + newProInfo.getPro_grouping_endHms();
+
+        createProjectDAO.inseart_project(bool_cross,bool_force,course_id,max_p,min_p,pro_name,teacher_id, group_ddl);
+
+
+        return new Void_return(20000);
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/vue-element-admin/teacher/project/new_project_insert2")
+    @ResponseBody
+    public Void_return create_new_project2(@RequestBody NewProjectReceive newProInfo){
+        int course_id = newProInfo.getCourse_id();
+        int min_p = newProInfo.getProject_member_limit().get(0);
+        int max_p = newProInfo.getProject_member_limit().get(1);
+        String token = newProInfo.getToken();
+        int teacher_id = upPersonInfoDAO.getID(token).get(0).getId();
         String pro_name = newProInfo.getProject_name();
         String bool_cross = newProInfo.getGrouping_freely();
         String bool_force = newProInfo.getChoosable_proj();
         List<String> all_tags = newProInfo.getAll_tags();
         List<String> project_pre_week = newProInfo.getProject_pre_week();
-        createProjectDAO.inseart_project(bool_cross,bool_force,course_id,max_p,min_p,pro_name);
-        String token = newProInfo.getToken();
+        String group_ddl = newProInfo.getPro_grouping_endDay() + " " + newProInfo.getPro_grouping_endHms();
 
-        int teacher_id = upPersonInfoDAO.getID(token).get(0).getId();
-        List<Integer> people_ids;
-
-
+        int pro_id = createProjectDAO.getProject_id(bool_cross,bool_force,course_id,max_p,min_p,pro_name,teacher_id).get(0).getId();
+        List<The_id> people_ids = createProjectDAO.getAllPeopleInCourse(course_id);
         if(bool_force.equals("true")){
             // 把这个课程下所有的  人 拉进 want people
-        }else{
-            //不用插入
+            for (The_id person_id:people_ids){
+                createProjectDAO.insert_wantPerson(person_id.getId(),pro_id);
+            }
         }
-
         for(String t: all_tags){
             // 把所有 tag 插入 tags 表格中
+            createProjectDAO.insert_tags(pro_id,t);
         }
-
-        for(String we: project_pre_week){
+        for(String week: project_pre_week){
             // 把所有 week 插入 checkPoint 表格中
+            createProjectDAO.insert_tags(week,pro_id);
         }
-
-
         return new Void_return(20000);
     }
+
+
 
 
 }
--- wj2/src/main/java/com/evan/wj/dao/CreateProjectDAO.java
+++ wj2/src/main/java/com/evan/wj/dao/CreateProjectDAO.java
@@ -15,10 +15,11 @@ public interface CreateProjectDAO extends JpaRepository<User,Integer> {
     @Query("select new com.evan.wj.bean.AllCourse(cor.id,cor.course_name) from PersonClass pc join People p on p.id = pc.person join Class c on c.id = pc.class1 join Course cor on cor.id = c.course where p.stu_id = ?1")
     List<AllCourse> getAllCourse(String token);
 
+
     @Transactional
     @Modifying
-    @Query(value = "insert into project (bool_cross_class, bool_force_join, course, max_people, min_people, name) VALUES (?1,?2,?3,?4,?5,?6)", nativeQuery = true)
-    void inseart_project(String bool_cross, String bool_force,int course_id ,int max, int min, String course_name);
+    @Query(value = "insert into project (bool_cross_class, bool_force_join, course, max_people, min_people, name, teacher_id, grouping_ddl) VALUES (?1,?2,?3,?4,?5,?6,?7,?8)", nativeQuery = true)
+    void inseart_project(String bool_cross, String bool_force,int course_id ,int max, int min, String course_name,int teacher_id, String grouping_ddl);
 
 
     @Transactional
@@ -26,9 +27,25 @@ public interface CreateProjectDAO extends JpaRepository<User,Integer> {
     @Query(value = "insert into want_person (gro_status, person, project, text) values ('未组队',?1,?2,'');", nativeQuery = true)
     void insert_wantPerson(int person_id, int project_id);
 
+    @Transactional
+    @Modifying
+    @Query(value = "insert into tag (project, tag) VALUES (?1,?2);", nativeQuery = true)
+    void insert_tags(int pro_id, String tag);
+
+
 
     @Query("select new com.evan.wj.bean.The_id(p.id) from PersonClass pc join Class c on c.id = pc.class1 join People p on p.id = pc.person where c.course = ?1 and pc.class_authority = 'student'")
     List<The_id> getAllPeopleInCourse(int course_id);
 
+
+    @Query("select new com.evan.wj.bean.The_id(p.id) from Project p where p.bool_cross_class = ?1 and p.bool_force_join = ?2 and p.course = ?3 and p.max_people = ?4 and p.min_people = ?5 and p.name = ?6 and p.teacher_id = ?7")
+    List<The_id> getProject_id(String bool_cross, String bool_force,int course_id ,int max, int min, String course_name,int teacher_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into check_point (pre_week, project, text) values (?1,?2,'')", nativeQuery = true)
+    void insert_tags(String week, int pro_id);
+
+    
 
 }
