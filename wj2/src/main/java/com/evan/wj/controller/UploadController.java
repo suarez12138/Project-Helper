71f26f6854cbdd13d368cea6428836cc5c58b498
Pikayue11 <11812715@mail.sustech.edu.cn>
2020-12-21 22:47:34 +0800

    upload

---
+++ wj2/src/main/java/com/evan/wj/controller/UploadController.java
@@ -0,0 +1,78 @@
+package com.evan.wj.controller;
+
+import com.evan.wj.result.Message_return;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+@Controller
+public class UploadController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
+
+    @PostMapping("/vue-element-admin/student/project/upload_file")
+    @ResponseBody
+    public Message_return upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return new Message_return(20000,"Failed! Please select the file!");
+        }
+        String fileName = file.getOriginalFilename();
+        String filePath = "/Users/itinypocket/workspace/temp/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            LOGGER.info("上传成功");
+            return new Message_return(20000,"Success!");
+        } catch (IOException e) {
+            LOGGER.error(e.toString(), e);
+        }
+        return new Message_return(20000,"Failed!");
+    }
+//
+//    @PostMapping("/multiUpload")
+//    @ResponseBody
+//    public String multiUpload(HttpServletRequest request) {
+//        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+//        String filePath = "/Users/itinypocket/workspace/temp/";
+//        for (int i = 0; i < files.size(); i++) {
+//            MultipartFile file = files.get(i);
+//            if (file.isEmpty()) {
+//                return "上传第" + (i++) + "个文件失败";
+//            }
+//            String fileName = file.getOriginalFilename();
+//
+//            File dest = new File(filePath + fileName);
+//            try {
+//                file.transferTo(dest);
+//                LOGGER.info("第" + (i + 1) + "个文件上传成功");
+//            } catch (IOException e) {
+//                LOGGER.error(e.toString(), e);
+//                return "上传第" + (i++) + "个文件失败";
+//            }
+//        }
+//
+//        return "上传成功";
+//
+//    }
+}
+
+
+
+
+
+
+
+
